62453f9aee8b439c7fcc115ead2ebbd6b446ec06
Mastersam <abadasamuelosp@gmail.com>
2020-04-01 08:53:26 -0700

    delays

--- scripts/scraper.py
+++ scripts/scraper.py
@@ -1,19 +1,26 @@
 from bs4 import BeautifulSoup
 import requests
 import numpy as np
+import time
+
+delays = [12, 3, 9, 21, 5, 6, 19, 7, 33, 11, 2, 17, 4]
 
 
 def get_random_ua():
+    delay = np.random.choice(delays)
+    time.sleep(delay)
+
     random_ua = ''
     ua_file = 'ua_file.txt'
     try:
         with open(ua_file) as f:
             lines = f.readlines()
         if len(lines) > 0:
-            prng = np.random.RandomState()
-            index = prng.permutation(len(lines) - 1)
-            idx = np.asarray(index, dtype=np.integer)[0]
-            random_proxy = lines[int(idx)]
+            while not random_ua.strip():
+                prng = np.random.RandomState()
+                index = prng.permutation(len(lines) - 1)
+                idx = np.asarray(index, dtype=np.integer)[0]
+                random_proxy = lines[int(idx)]
     except Exception as ex:
         print('Exception in random_ua')
         print(str(ex))
