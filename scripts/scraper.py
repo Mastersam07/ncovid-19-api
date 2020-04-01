38d0312c4441fc76226fcef0939716ef0e9d343f
Mastersam <abadasamuelosp@gmail.com>
2020-04-01 08:39:50 -0700

    added ua file

--- scripts/scraper.py
+++ scripts/scraper.py
@@ -1,8 +1,20 @@
 from bs4 import BeautifulSoup
 import requests
+import lxml.html as lh
+import pandas as pd
 
 url = "https://futa.edu.ng/"
-response = requests.get("https://covid19.ncdc.gov.ng")
+headers = {
+        'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) '
+                      'Chrome/56.0.2924.87 Safari/537.36',
+    }
+response = requests.get("https://covid19.ncdc.gov.ng", headers=headers)
 content = BeautifulSoup(response.content, "html.parser")
 
-print(content)
+print(content.prettify())
+
+tb = content.find('table', id_='custom3')
+
+for link in tb.find_all('b'):
+    name = link.find('a')
+    print(name)
