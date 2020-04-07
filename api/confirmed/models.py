e61cae8a03a3d6ee9a0067964c898e429ddc5eea
Mastersam <abadasamuelosp@gmail.com>
2020-04-06 17:09:16 -0700

    model edit

--- api/confirmed/models.py
+++ api/confirmed/models.py
@@ -3,9 +3,9 @@ from django.db import models
 
 class Confirmed(models.Model):
     id = models.IntegerField(primary_key=True)
-    categories = models.CharField(db_column='Categories', max_length=13, blank=True,
+    categories = models.CharField(db_column='Categories', max_length=50, blank=True,
                                   null=True)  # Field name made lowercase.
-    values = models.CharField(db_column='Values', max_length=3, blank=True, null=True)  # Field name made lowercase.
+    values = models.CharField(db_column='Values', max_length=50, blank=True, null=True)  # Field name made lowercase.
 
     def __str__(self):
         return self.categories
