1b3ad8514109d9c205b01faa20f2016f3c61906f
Mastersam <abadasamuelosp@gmail.com>
2020-04-08 08:45:24 -0700

    typo fixes

--- api/daily/urls.py
+++ api/daily/urls.py
@@ -4,10 +4,10 @@ from rest_framework.routers import DefaultRouter
 from .apiviews import DailyList, DailyDetail
 
 router = DefaultRouter()
-router.register('api/states', DailyList, base_name='daily')
+router.register('api/daily', DailyList, base_name='daily')
 
 urlpatterns = [
     path("api/daily/", DailyList.as_view(), name="daily_list"),
-    path("api/daily/<str:date>/", DailyDetail.as_view(), name="daily_detail"),
+    path("api/daily/<int:id>/", DailyDetail.as_view(), name="daily_detail"),
 ]
 
