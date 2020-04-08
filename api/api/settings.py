61088d7bd0a7eb6facdc51aebc12e75df1751d20
Mastersam <abadasamuelosp@gmail.com>
2020-04-08 09:50:10 -0700

    extras for static files

--- api/api/settings.py
+++ api/api/settings.py
@@ -143,3 +143,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
